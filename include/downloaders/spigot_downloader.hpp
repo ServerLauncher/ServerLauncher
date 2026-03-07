c49fb72fbbd2c0d02b89b3075efdab4d17b80f6b
Waxby <waxby777@gmail.com>
2026-03-07 21:01:25 +0300

    Added spigot_downloader

---
+++ include/downloaders/spigot_downloader.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "downloader.hpp"
+
+struct SpigotVersion : public VersionInfo {
+    SpigotVersion(std::string version): version(version)
+    {}
+    
+    std::string version;
+};
+
+class SpigotDownloader : public Downloader {
+public:
+    const VersionList& getListOfMcVer() override;
+    const LoaderVersionList& getListOfLoaderVer(const std::string& mc_version) override {
+        throw std::logic_error("Spigot does not have separate loader versions");
+    }
+    void downloadVersion(const VersionInfo& version) override {
+        throw std::logic_error("Use downloadBuild instead of it");
+    }
+    void downloadBuild(const VersionInfo& version);
+private:
+    VersionList mc_cache;
+    const std::string mc_version_url = "https://hub.spigotmc.org/nexus/content/repositories/snapshots/org/spigotmc/spigot-api/maven-metadata.xml";
+    const std::string build_url = "https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar";
+};
---
+++ src/downloaders/spigot_downloader.cpp
@@ -0,0 +1,55 @@
+#include <string>
+#include <regex>
+#include <filesystem>
+#include <cpr/cpr.h>
+#include <spdlog/spdlog.h>
+#include <cstdlib>
+
+#include "spigot_downloader.hpp"
+
+const VersionList& SpigotDownloader::getListOfMcVer() {
+    cpr::Response r = cpr::Get(cpr::Url(mc_version_url));
+
+    if(r.status_code == 200) {
+        std::regex version_pattern(R"(<version>([\d]+\.[\d]+(?:\.[\d]+)?)[^<]*<\/version>)");
+        auto begin = std::sregex_iterator(r.text.begin(), r.text.end(), version_pattern);
+        auto end = std::sregex_iterator();
+
+        std::unordered_set<std::string> seen_versions;
+        for (auto i = begin; i != end; ++i) {
+            std::string ver = (*i)[1].str();
+            if(seen_versions.insert(ver).second) { //If it's not already seen
+                mc_cache.arr.push_back(ver);
+            }
+        }
+        spdlog::info("Fetched Minecraft versions(Spigot)");
+    } else {
+        spdlog::error("Failed to fetch Minecraft versions(Spigot). Status code: {}, Message: {}", r.status_code, r.error.message);
+        throw std::runtime_error("Failed to fetch Minecraft versions. Status code: " + std::to_string(r.status_code));
+    }
+
+    return mc_cache;
+}
+void SpigotDownloader::downloadBuild(const VersionInfo& version) {
+    const SpigotVersion& ver = static_cast<const SpigotVersion&>(version);
+    std::string path = instances_dir + "BuildTools.jar";
+
+    if(!std::filesystem::exists(path)){
+        cpr::Response r = cpr::Get(cpr::Url(build_url));
+        if (r.status_code == 200) {
+            std::ofstream out(path, std::ios::binary);
+            out.write(r.text.c_str(), r.text.size());
+            out.close();
+            spdlog::info("Downloaded to {}", path);
+        } else{
+            spdlog::error("Failed to download version: {}. Status code: {}, Message: {}", ver.version, r.status_code, r.error.message);        
+            throw std::runtime_error(std::to_string(r.status_code) + " " + r.error.message);
+        }
+    } else {
+        spdlog::info("BuildTools already exists at {}", path);
+    }
+    std::filesystem::current_path(instances_dir);
+    std::string cmd = "java -jar BuildTools.jar --rev " + ver.version;
+    spdlog::info("Running BuildTools for version {}", ver.version);
+    std::system(cmd.c_str());
+}
