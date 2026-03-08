65a48fbaa0816a388062b1510dbe646774ff8f2e
Waxby <waxby777@gmail.com>
2026-03-08 19:21:17 +0300

    Added directory checking

--- include/downloader.hpp
+++ include/downloader.hpp
@@ -30,16 +30,20 @@ inline std::ostream& operator<<(std::ostream& os, const T& list) {
 class Downloader {
 public:
     Downloader() {
-        const char* appdata = std::getenv("LOCALAPPDATA");
-        if (!appdata) {
+        const char* localappdata = std::getenv("LOCALAPPDATA");
+        if (!localappdata) {
             throw std::runtime_error("LOCALAPPDATA environment variable not found");
         }
-        work_dir = std::string(appdata) + "/MSC/";
+        work_dir = std::string(localappdata) + "/MSC/";
         instances_dir = work_dir + "instances/";
-        std::filesystem::create_directories(work_dir);
-        spdlog::info("Created {} directory", work_dir);
-        std::filesystem::create_directories(instances_dir);
-        spdlog::info("Created {} directory", instances_dir);
+        if(!std::filesystem::exists(work_dir)){
+            std::filesystem::create_directories(work_dir);
+            spdlog::info("Created {} directory", work_dir);
+        }
+        if(!std::filesystem::exists(instances_dir)){
+            std::filesystem::create_directories(instances_dir);
+            spdlog::info("Created {} directory", instances_dir);
+        }
     }
     virtual const LoaderVersionList& getListOfLoaderVer(const std::string& mc_version) = 0;
     virtual void downloadVersion(const VersionInfo& version) = 0;
