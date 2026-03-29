2a9ef7701729485ffefbf3ac10f1c08b7ff98003
Waxby <waxby777@gmail.com>
2026-03-29 18:13:21 +0300

    Rename LoaderVersionList to BuildList Rename loader_cache to build_cache Rename getListOfLoaderVer() to getListOfBuild()

--- include/downloaders/purpur_downloader.hpp
+++ include/downloaders/purpur_downloader.hpp
@@ -15,10 +15,10 @@ struct PurpurVersion : public VersionInfo {
 class PurpurDownloader : public Downloader {
 public:
     const VersionList& getListOfMcVer() override;
-    const LoaderVersionList& getListOfLoaderVer(const std::string& mc_version) override;
+    const BuildList& getListOfBuild(const std::string& mc_version) override;
     void downloadVersion(const VersionInfo& version) override;
 private:
     VersionList mc_cache;
-    LoaderVersionList loader_cache;
+    BuildList build_cache;
     const std::string url = "https://api.purpurmc.org/v2/purpur/";
 };
