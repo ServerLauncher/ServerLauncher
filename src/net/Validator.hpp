f99b36ccfca648219c4243c942b49a4cb71aa4b5
Waxby <waxby777@gmail.com>
2026-05-29 20:53:35 +0300

    refactor: add setErrorMessage method & errorMessage

--- src/net/Validator.hpp
+++ src/net/Validator.hpp
@@ -12,4 +12,14 @@ public:
     virtual bool write(QByteArray* data) = 0;
     virtual bool abort() = 0;
     virtual bool validate(QNetworkReply& reply) = 0;
+
+    const QString& errorMessage() const { return m_errorMessage; }
+
+protected:
+    void setErrorMessage(const QString& errorMessage){
+        m_errorMessage = errorMessage; 
+    }
+
+private:
+    QString m_errorMessage;
 };
