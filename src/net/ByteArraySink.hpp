e085cebcaea71abe8c1d0281e49530b71d5a447d
Waxby <waxby777@gmail.com>
2026-05-29 20:51:13 +0300

    refactor: add validator support to ByteArraySink

--- src/net/ByteArraySink.hpp
+++ src/net/ByteArraySink.hpp
@@ -4,27 +4,50 @@
 
 class ByteArraySink : public Sink {
 public:
-    ByteArraySink(QByteArray* target): m_target(target) 
+    explicit ByteArraySink(QByteArray* target)
+        : m_target(target)
     { }
 
     Task::State init(QNetworkRequest& request) override {
+        if (!m_target) {
+            return Task::State::Failed;
+        }
+        m_target->clear();
+        if (!initAllValidators(request)) {
+            return Task::State::Failed;
+        }
         return Task::State::Running;
     }
+
     Task::State write(QByteArray* data) override {
+        if (!writeAllValidators(data)) {
+            return Task::State::Failed;
+        }
         m_target->append(*data);
         return Task::State::Running;
     }
-    Task::State abort() override {
-        m_target->clear();
-        return Task::State::Failed;
-    }
+
     Task::State finalize(QNetworkReply& reply) override {
+        if (!validateAllValidators(reply)) {
+            m_target->clear();
+            return Task::State::Failed;
+        }
+
         return Task::State::Completed;
     }
 
+    Task::State abort() override {
+        abortAllValidators();
+
+        if (m_target) {
+            m_target->clear();
+        }
+        return Task::State::Aborted;
+    }
+
     bool hasLocalData() override {
-        return false;
+        return m_target && !m_target->isEmpty();
     }
 private:
-    QByteArray* m_target;
+    QByteArray* m_target = nullptr;
 };
